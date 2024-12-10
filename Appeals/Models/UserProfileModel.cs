e094a50aebfb037f25e2d33e44a956eaa87b117a
Lazar <LazarevJulia201@yandex.ru>
2024-12-10 14:02:50 +0300

    -

--- Appeals/Models/UserProfileModel.cs
+++ Appeals/Models/UserProfileModel.cs
@@ -5,10 +5,9 @@ namespace Appeals.Models
 {
     public class UserProfileModel
     {
-        [Required(ErrorMessageResourceType = typeof(Resources.UserProfile), ErrorMessageResourceName = "Required")]
+       
         public string Name { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(Resources.UserProfile), ErrorMessageResourceName = "Required")]
         public string Surname { get; set; }
 
         public string MiddleName { get; set; }
@@ -16,20 +15,17 @@ namespace Appeals.Models
         [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(Resources.UserProfile), ErrorMessageResourceName = "Required")]
-        [EmailAddress(ErrorMessageResourceType = typeof(Resources.UserProfile), ErrorMessageResourceName = "InvalidEmail")]
         public string Email { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(Resources.UserProfile), ErrorMessageResourceName = "Required")]
+        
         public string City { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(Resources.UserProfile), ErrorMessageResourceName = "Required")]
+       
         public string Street { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(Resources.UserProfile), ErrorMessageResourceName = "Required")]
+       
         public string House { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(Resources.UserProfile), ErrorMessageResourceName = "Required")]
         public string Apartment { get; set; }
     }
 }
