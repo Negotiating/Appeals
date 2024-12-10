f30296683da56b66d0c8231272ff981874705e44
Lazar <LazarevJulia201@yandex.ru>
2024-12-10 18:08:28 +0300

    Revert "стили"
    This reverts commit adbb2290b33e15efa769afaa4b88c455766f2c2c.

--- Appeals/Models/UserProfileModel.cs
+++ Appeals/Models/UserProfileModel.cs
@@ -5,17 +5,27 @@ namespace Appeals.Models
 {
     public class UserProfileModel
     {
-        public int Id { get; set; }
+       
         public string Name { get; set; }
+
         public string Surname { get; set; }
+
         public string MiddleName { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
-        public string Email { get; set; }
-        public string City { get; set; }
-        public string Street { get; set; }
-        public string House { get; set; }
-        public string Apartment { get; set; }
 
+        public string Email { get; set; }
+
+        
+        public string City { get; set; }
+
+       
+        public string Street { get; set; }
+
+       
+        public string House { get; set; }
+
+        public string Apartment { get; set; }
     }
 }
